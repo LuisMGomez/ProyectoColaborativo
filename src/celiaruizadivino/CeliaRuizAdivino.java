4ad3c4c3a5300ab2fa705dce02728da28acb6855
Celia <Celia@Celia-PC>
2019-11-09 14:18:01 +0100

    modifico codigo

--- src/celiaruizadivino/CeliaRuizAdivino.java
+++ src/celiaruizadivino/CeliaRuizAdivino.java
@@ -17,8 +17,8 @@ public class CeliaRuizAdivino {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int n = 10,cont = 10;
+        int x = (int) (50 *Math.random());
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
